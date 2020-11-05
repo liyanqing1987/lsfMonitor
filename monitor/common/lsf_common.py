245f8b68c807a93afe04510ae9d3561e44ace7d7
liyanqing1987 <liyanqing1987@163.com>
2020-11-05 03:44:54 -0800

    fix a bug on how to get job command

--- monitor/common/lsf_common.py
+++ monitor/common/lsf_common.py
@@ -140,7 +140,7 @@ def getBjobsUfInfo(command='bjobs -u all -r -UF'):
                      'projectCompile'             : re.compile('.*Project <([^>]+)>.*'),
                      'statusCompile'              : re.compile('.*Status <([A-Z]+)>*'),
                      'queueCompile'               : re.compile('.*Queue <([^>]+)>.*'),
-                     'commandCompile'             : re.compile('.*Command <(.+)>\s*$'),
+                     'commandCompile'             : re.compile('.*Command <(.+?)>.*$'),
                      'submittedFromCompile'       : re.compile('.*Submitted from host <([^>]+)>.*'),
                      'submittedTimeCompile'       : re.compile('(.*): Submitted from host.*'),
                      'cwdCompile'                 : re.compile('.*CWD <([^>]+)>.*'),
